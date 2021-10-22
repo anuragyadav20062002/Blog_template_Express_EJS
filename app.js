0bd3e635cc2a8a70c4a5d75b8d447ce879596334
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-10-22 23:45:57 +0530

    added about page

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(express.static("public"))
 app.get("/", (req, res) => {
   res.render("home", { hometext: homeStartingContent })
 })
+app.get("/about", (req, res) => {
+  res.render("about", { abouttext: aboutContent })
+})
+app.get("/contact", (req, res) => {
+  res.render("contact", { contacttext: contactContent })
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000")
