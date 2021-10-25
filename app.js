211379075f59af5ecd66812c4f8d6a4fd8e35119
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-10-25 21:50:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const { lowerCase } = require("lodash")
-const mongoose = require("mongoose")
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
@@ -19,15 +18,6 @@ app.set("view engine", "ejs")
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
-
-mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true })
-
-const postschema = {
-  title: String,
-  content: String,
-}
-
-const Post = mongoose.model("Post", postschema)
 
 var posts = []
 
@@ -64,13 +54,12 @@ app.post("/compose", (req, res) => {
   const blogtitle = req.body.posttitle
   const blogcontent = req.body.postbody
 
-  const post = new Post({
-    title: req.body.posttitle,
+  const post = {
+    title: blogtitle,
+    body: blogcontent,
+  }
 
-    content: req.body.postbody,
-  })
-
-  post.save()
+  posts.push(post)
 
   res.redirect("/")
 })
